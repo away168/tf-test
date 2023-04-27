fe5450be84e06f8bf0d94599d761173346ee326d
Alexey Popovich <alexey.popovich@bluescape.com>
2023-04-27 11:31:53 -0400

    moving to remote state

--- project-test/main.tf
+++ project-test/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "remote" {
+    hostname     = "backend.api.env0.com"
+    organization = "9edac48b-0c62-45d9-9dfc-131deb39979b"
+    workspaces {
+      name = "project-test"
+    }
+  }
+}
+
 module "test_module" {
     source = "../modules/test-module"
     
