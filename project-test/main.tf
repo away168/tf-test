0bb1a64f68567611ff40036f59062a91a3c7c519
Andrew Way <andrew.way@env0.com>
2023-05-11 13:48:00 -0700

    use main.tf

--- project-test/main.tf
+++ project-test/main.tf
@@ -1,12 +1,31 @@
+# terraform {
+#   backend "remote" {
+#     hostname     = "backend.api.env0.com"
+#     organization = "bde19c6d-d0dc-4b11-a951-8f43fe49db92.5572ceeb-c526-4152-b6de-ea8f3adfc4f1"
+#     workspaces {
+#       name = "project-test"
+#     }
+#   }
+# }
+
 terraform {
   backend "remote" {
-    hostname     = "backend.api.env0.com"
-    organization = "9edac48b-0c62-45d9-9dfc-131deb39979b"
+    organization = "wayfamily"
     workspaces {
-      name = "project-test"
+      name = "tf-test"
     }
   }
 }
+
+# terraform {
+#   cloud {
+#     organization = "wayfamily"
+
+#     workspaces {
+#       name = "tf-test"
+#     }
+#   }
+# }
 
 module "test_module" {
     source = "../modules/test-module"
